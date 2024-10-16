88758e2379c9a3225522cdb8f041db3f59725614
DillanDevs <dillansdev@gmail.com>
2024-10-15 19:18:28 -0500

    First commit

---
+++ batch/src/main/java/co/com/asprilla/batch/BatchService.java
@@ -0,0 +1,33 @@
+package co.com.asprilla.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class BatchService {
+
+    private static final Logger logger = LoggerFactory.getLogger(BatchService.class);
+
+    private final RestTemplate restTemplate;
+
+    public BatchService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Scheduled(fixedRate = 120000) // 120000 ms = 2 minutos
+    public void executeBatchProcess() {
+        logger.info("Iniciando proceso batch...");
+        try {
+            // Realizar la llamada HTTP al servicio orquestador
+            // Puedes configurar la URL en application.properties o hardcodearla
+            String orchestratorUrl = "http://host.docker.internal:9000/orchestrator/parallel-responses";
+            String response = restTemplate.getForObject(orchestratorUrl, String.class);
+            logger.info("Respuesta del orquestador: {}", response);
+        } catch (Exception e) {
+            logger.error("Error al llamar al orquestador: ", e);
+        }
+    }
+}
